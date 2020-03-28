1d699f288b4929cade665e78ebb1d66a7dceb906
wangbinbin <wangbinsf@163.com>
2020-03-28 09:54:41 +0800

    initialize project

---
+++ WechatMoments/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WechatMoments
+//
+//  Created by 王宾宾 on 2020/3/28.
+//  Copyright © 2020 王宾宾. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
